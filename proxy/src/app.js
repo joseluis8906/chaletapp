269539f49f2eceb5f37d5294a6d84d240db76ee1
Jose Cáceres <joseluis8906@opmbx.org>
2017-09-28 22:21:08 -0500

    actualizado

--- proxy/src/app.js
+++ proxy/src/app.js
@@ -2,5 +2,5 @@ import redbird from 'redbird';
 
 var proxy = redbird({port: 3000});
 
-proxy.register('localhost:3000/', 'http://127.0.0.1:3001/');
-proxy.register('localhost:3000/backend', 'http://127.0.0.1:3002/');
+proxy.register('localhost:3000/', 'http://127.0.0.1:3003/');
+proxy.register('localhost:3000/backend', 'http://127.0.0.1:3004/');
