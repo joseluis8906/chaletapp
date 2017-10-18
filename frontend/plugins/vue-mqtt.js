f3527e94f27b8f181f18a250e3b3c4a2e9d530ee
Jose Cáceres <joseluis8906@opmbx.org>
2017-10-18 11:43:00 -0500

    actualizado

--- frontend/plugins/vue-mqtt.js
+++ frontend/plugins/vue-mqtt.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import VueMqtt from 'vue-mqtt'
 
-Vue.use(VueMqtt, "ws://149.56.109.238:9001", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
+Vue.use(VueMqtt, "ws://149.56.109.238:9001/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
