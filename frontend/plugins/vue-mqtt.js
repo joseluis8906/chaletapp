4ef5fc888c8317dfb277b1cd2d8a03a26f38c77c
Jose Cáceres <joseluis8906@opmbx.org>
2017-10-27 12:21:21 -0500

    actualizado

--- frontend/plugins/vue-mqtt.js
+++ frontend/plugins/vue-mqtt.js
@@ -9,7 +9,7 @@ web 56784321
 */
 
 //vps de prueba
-//Vue.use(VueMqtt, "http://test1.unixjs.com/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
+//Vue.use(VueMqtt, "http://test1.unixjs.com/ws/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
 //local host
 Vue.use(VueMqtt, "http://site.com/ws/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
 // vps chalet
