7cc395e16ab821d25f836187ece2c14027719ff8
Jose Cáceres <joseluis8906@opmbx.org>
2017-10-30 13:50:14 -0500

    actualizado

--- frontend/plugins/vue-mqtt.js
+++ frontend/plugins/vue-mqtt.js
@@ -11,6 +11,6 @@ web 56784321
 //vps de prueba
 //Vue.use(VueMqtt, "http://test1.unixjs.com/ws/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
 //local host
-Vue.use(VueMqtt, "http://site.com/ws/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
+//Vue.use(VueMqtt, "http://site.com/ws/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
 // vps chalet
-//Vue.use(VueMqtt, "wss://reservaselgranchalet.com:443/ws/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
+Vue.use(VueMqtt, "wss://reservaselgranchalet.com:443/ws/mqtt", {username: 'web', password: '56784321', clientId: 'chaletApp_' + Math.random().toString(16).substr(2, 8)})
