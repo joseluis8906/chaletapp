0869b371232072fb86f38e4fcaadde9eaa0f8e34
Jose Cáceres <joseluis8906@opmbx.org>
2017-10-30 13:44:32 -0500

    actualizado

--- frontend/pages/recargas.vue
+++ frontend/pages/recargas.vue
@@ -329,7 +329,7 @@ export default {
         if(this.UsuarioId === null || this.EmpleadoId === null){
           this.$store.commit('notificaciones/changeContext', 'error')
           this.$store.commit('notificaciones/changeIcon', 'error_outline')
-          this.$store.commit('notificaciones/changeMsg', 'Error general\nsalga y vuelva a entrar.')
+          this.$store.commit('notificaciones/changeMsg', 'Error general\nsalga y vuelva a entrar')
           this.$store.commit('notificaciones/changeState', 1)
           return null;
         }
